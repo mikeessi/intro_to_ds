82aa814836d08e5b3c86cae225d14401d73fbe99
Mikko Anttonen <mikkohj.anttonen@gmail.com>
2024-10-19 17:50:37 +0300

    Add base.html and ph pages

--- src/routes.py
+++ src/routes.py
@@ -5,3 +5,11 @@ from src import app
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/statistics")
+def statistics():
+    return render_template("statistics.html")
+
+@app.route("/predictions")
+def predictions():
+    return render_template("predictions.html")
